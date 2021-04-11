649dc5920c1b5db4e540821d3d4f4a3294f11703
mo-anis-agwan <anishagwan123@gmail.com>
2021-04-11 15:50:31 +0530

    Made changes for support of iOS 12, Made the UI cahnges

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //MARK: - Vars
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
-
+    
+    
+    //MARK: - IBACtions
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
 
 }
 
